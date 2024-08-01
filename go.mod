1c2ddafcbfeba3db5f48be5d3d5d3cc3639d92b0
Matthew Boyle <hello@mattjamesboyle.com>
2024-08-01 14:34:34 +0100

    checkpoint-3

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/matthewjamesboyle/gophercon-2024
 
 go 1.22.5
 
-require github.com/uhthomas/slogctx v0.0.0-20230822204520-4d0b73cf68e7 // indirect
+require (
+	github.com/VictoriaMetrics/metrics v1.35.1
+	github.com/uhthomas/slogctx v0.0.0-20230822204520-4d0b73cf68e7
+)
+
+require (
+	github.com/valyala/fastrand v1.1.0 // indirect
+	github.com/valyala/histogram v1.2.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
--- internal/transporthttp/router.go
+++ internal/transporthttp/router.go
@@ -4,42 +4,69 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/VictoriaMetrics/metrics"
 	"github.com/matthewjamesboyle/gophercon-2024/internal/recomendation"
 	"github.com/uhthomas/slogctx"
 	"net/http"
 	"strconv"
+	"time"
+)
+
+var (
+	requestsTotal   = "requests_total{status_code=%d}"
+	requestDuration = metrics.NewSummary("request_duration_seconds")
 )
 
 func NewMux(ctx context.Context, svc *recomendation.Service) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /trip/{budget}", func(w http.ResponseWriter, r *http.Request) {
-		budget := r.PathValue("budget")
+	mux.HandleFunc("/trip/", func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		defer func() {
+			recordDuration(startTime)
+		}()
+
+		// Extract budget from query parameters and parse it
+		budget := r.URL.Query().Get("budget")
 		b, err := strconv.ParseInt(budget, 10, 64)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			metrics.GetOrCreateCounter(fmt.Sprintf(requestsTotal, http.StatusBadRequest))
 			return
 		}
 
+		// Get recommendation
 		rec, err := svc.Get(ctx, int(b))
 		if err != nil {
 			switch {
 			case errors.Is(err, recomendation.ErrBudgetOutOfBounds):
 				w.WriteHeader(http.StatusBadRequest)
-				return
+				metrics.GetOrCreateCounter(fmt.Sprintf(requestsTotal, http.StatusBadRequest))
 			default:
 				slogctx.From(ctx).Error("unhandled error", err)
 				w.WriteHeader(http.StatusInternalServerError)
-				return
+				metrics.GetOrCreateCounter(fmt.Sprintf(requestsTotal, http.StatusInternalServerError))
 			}
+			return
 		}
 
+		// Marshal response
 		res, err := json.Marshal(rec)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			metrics.GetOrCreateCounter(fmt.Sprintf(requestsTotal, http.StatusInternalServerError))
 			return
 		}
+
+		w.WriteHeader(http.StatusOK)
+		metrics.GetOrCreateCounter(fmt.Sprintf(requestsTotal, http.StatusOK))
 		_, _ = w.Write(res)
 	})
 
 	return mux
+}
+
+func recordDuration(startTime time.Time) {
+	duration := time.Since(startTime).Seconds()
+	requestDuration.Update(duration)
 }
