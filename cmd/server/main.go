18c881640360f18f456ce29a45417c7c63a2773f
Matthew Boyle <hello@mattjamesboyle.com>
2024-08-02 11:08:44 +0100

    tracing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/matthewjamesboyle/gophercon-2024/internal/recomendation"
 	"github.com/matthewjamesboyle/gophercon-2024/internal/transporthttp"
 	"github.com/uhthomas/slogctx"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	trace2 "go.opentelemetry.io/otel/trace"
 	"log"
 	"log/slog"
 	"net/http"
@@ -14,9 +20,29 @@ import (
 	"time"
 )
 
+var tracer trace2.Tracer
+
 func main() {
 
 	ctx := context.Background()
+
+	exporter, err := jaeger.New(
+		jaeger.WithCollectorEndpoint(),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create Trace Provider
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("app-one"),
+		)),
+	)
+
+	otel.SetTracerProvider(tp)
+	tracer = tp.Tracer("app-one")
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	ctx = slogctx.With(ctx, logger)
--- go.mod
+++ go.mod
@@ -5,10 +5,17 @@ go 1.22.5
 require (
 	github.com/VictoriaMetrics/metrics v1.35.1
 	github.com/uhthomas/slogctx v0.0.0-20230822204520-4d0b73cf68e7
+	go.opentelemetry.io/otel/exporters/jaeger v1.17.0
 )
 
 require (
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/valyala/fastrand v1.1.0 // indirect
 	github.com/valyala/histogram v1.2.0 // indirect
+	go.opentelemetry.io/otel v1.17.0 // indirect
+	go.opentelemetry.io/otel/metric v1.17.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.17.0 // indirect
+	go.opentelemetry.io/otel/trace v1.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 )
